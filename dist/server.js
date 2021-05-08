01c16ebd224e92414a13033b443dc91271e096f9
ohmygaugh-crypto <sethhmcgaugh@gmail.com>
2021-05-08 17:07:48 -0400

    added other html files in a timeline arrangement as quicksaves: old, index(current), new

--- dist/server.js
+++ dist/server.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
     res.render('index');
 });
 
-app.listen(8080, () => {
-    console.log("Listening on http://localhost:8080");
+app.listen(8000, () => {
+    console.log("Listening on http://localhost:8000");
 });
