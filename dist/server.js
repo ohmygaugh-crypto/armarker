8f3e1d172a95f63d19efbf6efe30f803619c3a13
ohmygaugh-crypto <sethhmcgaugh@gmail.com>
2021-05-11 09:43:34 -0400

    removed all entities that caused performace issues, restructured layout and script order to maximize fps

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require("cors")
 
+app.use(cors())
 app.set("/", "html");
 app.use(express.static(path.join(__dirname, "/")));
 app.use(express.json());
